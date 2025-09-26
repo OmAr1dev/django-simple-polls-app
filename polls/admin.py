12854a01a026c2c484a6c3fbf643ed5e997949e2
Omar Soudani <omar.soudani.dev@gmail.com>
2025-09-26 14:05:55 +0100

    feat: register Poll, Choice, and Vote models in admin with inline choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Poll, Vote
+
+
+class ChoiceInline(admin.TabularInline):
+    """Custom admin configuration for the Poll model."""
+
+    model = Choice
+    extra = 2  # number of extra choices to show
+
+
+class PollAdmin(admin.ModelAdmin):
+    """Custom admin configuration for the Poll model."""
+
+    inlines = [ChoiceInline]
+    list_display = ("title", "created_at")
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Vote)
